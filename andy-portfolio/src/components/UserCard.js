26d1f607d6484943e55b3297b84bcb2784528136
andy51011 <andyyuen287@gmail.com>
2022-03-23 12:52:27 -0700

    added linkedin and github links

--- andy-portfolio/src/components/UserCard.js
+++ andy-portfolio/src/components/UserCard.js
@@ -1,24 +1,41 @@
-import React from "react";
-import "./userCard.css"
-import portrait from "./background/portrait.jpg"
-import { FaGithub} from "react-icons/fa";
-
+import React, { useState } from "react";
+import "./userCard.css";
+import portrait from "./background/portrait.jpg";
+import { FaGithub } from "react-icons/fa";
+import { AiFillLinkedin } from "react-icons/ai";
+import { FiGithub } from "react-icons/fi";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 const UserCard = ({ myInfo }) => {
+  const [isShown, setIsShown] = useState(true);
+  const [linkedin, setLinkedin] = useState(true);
+
+  const showIcon = (bool) => {
+      setIsShown(bool);
+  };
+
+  const showLinkedinIcon = (bool) => {
+      setLinkedin(bool);
+  };
+
   return (
     <div className="card-container">
       <div className="img-container">
-          <img className="usercard-img" src={portrait} alt="person-icon" />
+        <img className="usercard-img" src={portrait} alt="person-icon" />
       </div>
       <div className="card-info">
-        <ul className="list-info">
-          <li>{myInfo.name}</li>
-          <li>{myInfo.github}<FaGithub /></li>
-          <li>{myInfo.address}</li>
-          <li>{myInfo.interests}</li>
-          <li>{myInfo.skills}</li>
-          <li>{myInfo.linkedin}</li>
-        </ul>
+        <div className="item-name">{myInfo.name}</div>
+        <div id="item-card" className="item-github">
+          <a onMouseEnter={() => showIcon(false)} onMouseLeave={() => showIcon(true)}href={myInfo.github}>
+            {isShown === true ? <FaGithub /> : < FiGithub />}
+          </a>
+        </div>
+        <div id="item-card" className="item-linkedin">
+          <a onMouseEnter={() => showLinkedinIcon(false)} 
+             onMouseLeave={() => showLinkedinIcon(true)} href={myInfo.linkedin}>
+            {linkedin === true ? <AiFillLinkedin /> : <AiOutlineLinkedin / >}
+          </a>
+        </div>
       </div>
     </div>
   );
